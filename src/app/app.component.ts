4d2a6c49709dfdecc7fa825ee684e3a645ac83f4
LalindaSampath <diaslalinda@gmail.com>
2016-11-14 21:44:35 +0530

    Initial Commt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app works!';
+  employees: FirebaseListObservable<any[]>;
+  constructor(af: AngularFire) {
+    this.employees = af.database.list('/employees');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header.component';
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyBSJOM5DL0p-ZQnn3BVU03NN_Z9PORn7kc',
+  authDomain: 'testangularfire2-4f7dc.firebaseapp.com',
+  databaseURL: 'https://testangularfire2-4f7dc.firebaseio.com',
+  storageBucket: 'testangularfire2-4f7dc.appspot.com'
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, HeaderComponent]
 })
 export class AppModule { }
